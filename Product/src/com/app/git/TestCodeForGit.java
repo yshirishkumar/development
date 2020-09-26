718625c8d8e7a71e0ce59f858c4727e6ad7fd37f
yshirishkumar@gmail.com <yshirishkumar@gmail.com>
2020-09-26 15:49:28 +0530

    local branch coomit

--- Product/src/com/app/git/TestCodeForGit.java
+++ Product/src/com/app/git/TestCodeForGit.java
@@ -3,6 +3,7 @@ package com.app.git;
 public class TestCodeForGit {
 	public static void main(String[] args) {
 		System.out.println("this for main method");
+		System.out.println("branch-first");
 	}
 
 }
